4bbdce3e20ebbc3f335713cde28dffd6795c324c
aabazary <aabazary@gmail.com>
2021-08-06 23:17:15 -0700

    added functionality of the questions being answered, gives basic layout for answers, needs visual improvements

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# READMEgen
+# Stuff
+aabazary
+aabazary@gmail.com
+## Description
+This is a project test
+## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+npm i
+npm test
+## Usage
+nothing
+## Credits
+everything
+## License
+GPL 3.0
+## Badges
+## Features
+## How to Contribute
+## Tests
--- index.js
+++ index.js
@@ -1,13 +1,105 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// TODO: Create an array of questions for user input. Questions from Demo Video of HW
+const questions = [{
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+
+    {
+        type: 'email',
+        name: 'email',
+        message: 'What is your email address? ',
+
+    },
+
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your project\'s name?',
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project:',
+    },
+
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'What kind of license should your project have?(Use arrow keys)',
+        choices: ['MIT', 'GPL 3.0', 'Apache 2.0', 'BSD 3', 'none']
+
+    },
+
+    {
+        type: 'input',
+        name: 'dependencies',
+        message: 'What command should be run to install dependencies?(npm i)',
+
+    },
+
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to run tests? (npm test)',
+    },
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'What does the user need to know about using the repo?',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'What does the user need to know about contributing to the repo?',
+    },
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+// function writeToFile(fileName, data) {}
+const generateREADME = (answers) => 
+`# ${answers.name}
+${answers.username}
+${answers.email}
+## Description
+${answers.description}
+## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+${answers.dependencies}
+${answers.tests}
+## Usage
+${answers.instructions}
+## Credits
+${answers.contributions}
+## License
+${answers.licenses}
+## Badges
+## Features
+## How to Contribute
+## Tests
+`;
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const contentREADME = generateREADME(answers);
+
+            fs.writeFile('README.md', contentREADME, (err) =>
+                err ? console.log(err) : console.log('Successfully created README.md!')
+            );
+        })
+};
 
 // Function call to initialize app
-init();
+init()
